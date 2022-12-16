46f5416b744779a6702bea63dbfab7c1cb149f49
Marcel van Hassel <marcelvanhassel@gmail.com>
2022-12-16 12:09:18 +0100

    Not working at the moment, trying to implement first api call

--- src/app/modules/ordering/components/order-sandwich/order-sandwich.component.ts
+++ src/app/modules/ordering/components/order-sandwich/order-sandwich.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Order} from "../../../../models/order.model";
+import {OrderService} from "../../../../services/order.service";
 
 @Component({
   selector: 'app-order-sandwich',
@@ -10,8 +12,10 @@ export class OrderSandwichComponent implements OnInit{
   nameForm: FormGroup;
   nameUnfilled = true;
   wantsSandwichUnfilled = true;
+  myOrder: Order;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+              private orderService: OrderService) {
   }
 
   ngOnInit(): void {
@@ -21,9 +25,18 @@ export class OrderSandwichComponent implements OnInit{
   }
 
   submitName() {
-    this.nameUnfilled = false;
-    this.nameForm.controls['name'].disable();
-    console.log('send name')
+    if(this.nameForm.valid) {
+      let name = this.nameForm.get('name').value;
+      this.orderService.findTodaysUnfilledOrderByName(name).subscribe((data) =>
+      {
+        this.myOrder = data;
+      });
+    }
+    if(this.myOrder) {
+      this.nameUnfilled = false;
+      this.nameForm.controls['name'].disable();
+      console.log(this.myOrder);
+    }
   }
 
 
